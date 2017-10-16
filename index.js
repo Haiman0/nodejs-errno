c9080d97933676ae132701b461f52ff9406f55f0
haiman <haiman_o@163.com>
2017-10-16 19:20:51 +0800

    docs: improve comments

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+/**
+ * @module err-no
+ */
 var Container = require('./lib/Container')
 var Domain = require('./lib/Domain')
 
+/** 
+ * Class of Container
+ * @type {Container}
+ */
 exports.Container = Container
+
+/** 
+ * Class of Domain
+ * @type {Domain}
+ */
 exports.Domain = Domain
 
+/**
+ * Create an instance of Container.
+ * @param  {object} options 
+ * @return {Container} -  instance of Container
+ */
 exports.create = function (options){
 	return new Container(options)
 }
